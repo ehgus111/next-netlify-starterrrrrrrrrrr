152b56acfb607afaf39b412a7dd94075679c89db
ehgus111 <115925853+ehgus111@users.noreply.github.com>
2022-10-23 20:38:58 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="사장님의 사이트에 오신것을 환영합니다" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
