a89ab3365787aece4d869b2bf46c28ddb4a8b30e
ehgus111 <115925853+ehgus111@users.noreply.github.com>
2022-10-23 21:02:06 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
 
       <main>
         <Header title="사장님의 사이트에 오신것을 환영합니다" />
+    <img style="display: block;-webkit-user-select: none;margin: auto;background-color: hsl(0, 0%, 90%);transition: background-color 300ms;" src="https://i9.ytimg.com/vi/N7C8sjSNUWA/mq2.jpg?sqp=CJTX1JoG&amp;rs=AOn4CLA_QjNAMWGUNBus2m0Utdd9Um4OXw">
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
